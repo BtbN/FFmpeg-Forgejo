0e355d97b546a95060f488c9695927677d0351a5
Code <code@ffmpeg.org>
2025-07-25 22:08:06 +0000

    Switch to reflink based snapshot backup

--- backup.sh
+++ backup.sh
@@ -3,17 +3,26 @@ set -xe
 cd "$(dirname "$0")"
 source .env
 
+docker run --pull=always --rm -u root -w / \
+	--mount type=bind,src="$PWD",dst=/workdir \
+	ubuntu:latest rm -rf /workdir/cur_backup
+
 mkdir -p ~/backups
-mkdir -p cur_backup/{bucket,data}
+mkdir -p cur_backup/bucket
 rclone mount --use-server-modtime --read-only --allow-other forgejo:code-ffmpeg-storage/ ./cur_backup/bucket &
-trap "jobs -p | xargs -r kill; docker compose up -d; wait; rm -rf cur_backup" EXIT
+trap "jobs -p | xargs -r kill; docker compose up -d; wait; docker run --rm -u root --mount type=bind,src='$PWD',dst=/workdir ubuntu:latest rm -rf /workdir/cur_backup" EXIT
 
 cp .env cur_backup/envfile.env
 
 docker compose stop forgejo
-docker compose exec -T db mariadb-dump --opt --single-transaction --extended-insert -u "$FORGEJO_DB_USER" --password="$FORGEJO_DB_PASSWORD" "$FORGEJO_DB_DATABASE" | gzip > cur_backup/database.sql.gz
 
-docker run --pull=always --rm -u root -w / \
+docker compose exec -T db mariadb-dump --opt --single-transaction --extended-insert -u "$FORGEJO_DB_USER" --password="$FORGEJO_DB_PASSWORD" "$FORGEJO_DB_DATABASE" | gzip > cur_backup/database.sql.gz
+docker run --rm -u root -w / \
+	--mount type=bind,src=/,dst=/workdir \
+	ubuntu:latest cp -a --reflink=always "/workdir$FORGEJO_DATA_DIR" "/workdir$PWD/cur_backup/data"
+
+docker compose start forgejo
+
+docker run --rm -u root -w / \
 	--mount type=bind,src="$PWD"/cur_backup,dst=/cur_backup,readonly \
-	--mount type=bind,src="$FORGEJO_DATA_DIR",dst=/cur_backup/data,readonly \
 	ubuntu:latest tar --exclude=./cur_backup/bucket/repo-archive -zpc ./cur_backup > ~/backups/"$(date "+%s")".tar.gz
